5cd02e3e5cc50bf41083167d8b564814cf6e8d16
Braxton Mills <braxton25@gmail.com>
2021-03-19 01:35:51 -0500

    Pushing before converting to framer motion variants

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,24 +3,62 @@ import React from "react";
 import { Container, Button } from "react-bootstrap";
 import { motion } from "framer-motion";
 
+export const splashAnim = () => {
+  <motion.div
+    animate={{rotate: 360}}
+    transition={{duration: 2}}
+    />
+}
+
+const button = {
+  rest: { scale: 0.1, opacity: 0 },
+  show: { scale: 1, opacity: 1,
+    transition: {repeat: 0, delay: 2.5, duration:1},
+    transitionEnd: {scale: 1, opacity: 1} },
+  hover: { scale: 1.1,
+    transition: {duration:0.5} },
+  pressed: { scale: 0.95 }
+};
+
+/* const mainContent = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1,
+    transition: {staggerChildren: 1}}
+} */
 
 export default function Home() {
   return (
     <div className="splash">
       <div className="main-content">
-        <h1 className="text-center">Getting a new puppy or dog?</h1>
-        <h2 className="text-center">...or have a stubborn one and need help?</h2>
-        <Button variant="primary" className="splashButton mx-auto">GET STARTED</Button>
-        {/* <p className="lead text-left">
-          "Can my old dog learn how to behave?"
-        </p>
-        <p className="lead text-left">
-          "How can I help my anxious dog meet new people and other canine friends?"
-        </p>
-        <p className="lead text-left">
-          "How can I stop my puppy from crying all-night and peeing everywhere?"
-        </p>
-        <p className="lead text-left">"What do I do? Help!"</p> */}
+        {/* Reminder: for animations with multiple children, use 
+            'staggerChildren' if using same animation for each child
+            instead of 'delay' 
+            
+            */}
+        <motion.h1 
+          className="text-center"
+          initial={{ opacity: 0, y: 50}}
+          animate={{ opacity: 1, y: 0}}
+          transition={{ duration: 1}}
+          >
+            Getting a new puppy or dog?
+        </motion.h1>
+        <motion.h2 
+          className="text-center"
+          initial={{ opacity: 0, y: 50}}
+          animate={{ opacity: 1, y: 0}}
+          transition={{ duration: 1, delay: 1.5}}
+          >
+            ...or have a stubborn one and need help?
+        </motion.h2>
+        <motion.button
+          className="splashButton mx-auto text-center"
+          variants={button}
+          initial="rest"
+          animate="show"
+          whileHover="hover"
+          whileTap="pressed">
+        GET STARTED</motion.button>
       </div>
     </div>
   );
