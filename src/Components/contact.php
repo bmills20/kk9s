921fcb8e1c1496e649bf22f9c035e1559ec60627
Braxton Mills <braxton25@gmail.com>
2021-04-24 01:19:33 -0500

    Pushing PHP code before potentially switching to express/nodemailer for contact.js

--- src/Components/contact.php
+++ src/Components/contact.php
@@ -2,27 +2,110 @@
     // Allow http request from any origin(ating) page
     // Change to kk9s after implementation on local
     // https://stackoverflow.com/questions/1653308/access-control-allow-origin-multiple-origin-domains
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
+    if(isset($_SERVER['HTTP_ORIGIN'])) {
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header('Access-Control-Allow-Credentials: true');
         header('Access-Control-Max-Age: 86400');    // cache for 1 day
     }
+
+    // Get JSON with message details
+    $rest_json = file_get_contents("php://input");
+    $_POST = json_decode($rest_json, true);
+
+    $errors = array();
 
     // Access-Control headers are received during OPTIONS requests
     // OPTIONS requests (used for debugging purposes): 
     // The OPTIONS method is used to describe the communication options for the target resource.
     // Once fully integrated, change to POST: 
     // The POST method is used to submit an entity to the specified resource, often causing a change in state or side effects on the server.
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if(isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
             header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
 
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        if(isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
             header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        // Validate that fields are filled (and valid) before
+        // continuing to send email
+        if(empty($_POST['email'])) {
+            $errors[] = 'Email field is empty';
+        }
+        else {
+            $email = $_POST['email'];
+            // Validating email with PHP filter
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $errors[] = 'Invalid email';
+            }
+        }
+        if(empty($_POST['message'])) {
+            $errors[] = 'Message is empty';
+        }
+        else {
+            $message = $_POST['message'];
+        }
+
+        // If no errors, format email
+        if (empty($errors)) {
+            $date = date('j, F Y h:i A');
+
+            $emailBody = "
+            <html>
+            <head>
+                <title> New message from $email </title>
+            </head>
+            <body style=\"background-color: $fafafa;\">
+                <div style=\"padding:20px;\">
+                    Date: <span style=\"color:#888\">$date</span>
+                    <br>
+                    Email: <span style=\"color:#888\">$email</span>
+                    <br>
+                    Message: <div style=\"color:#888\">$message</div>
+                </div>
+                </body>
+                </html>
+            
+            ";
+
+            // PHP refresher: double quotes check for variable, single quotes do not
+            $headers = 'From: Contact Form' . "\r\n" .
+            "Reply-To $email" . "\r\n" .
+            // MIME version will always be 1.0
+            // MIME alows emails to contain characters other than ASCII characters
+            // In our case our email contains HTML styling
+            "MIME-Version: 1.0\r\n" .
+            "Content-Type: text/html; charset=iso-8859-1\r\n";
+
+            $to = 'me@meme.com';
+            $subject = 'Wassup, train my dog plz';
+
+            if(mail($to, $subject, $emailBody, $headers)) {
+                $sent = true;
+            }
+
+
+        }
+
 
         exit(0);
     }
 
     echo "You have CORS!";
 ?>
+
+<?php if (!empty($errors)) : ?>
+
+{
+  "status": "fail",
+  "error":  <?php echo json_encode($errors) ?>
+}
+  <?php endif; ?>
+
+
+  <?php if (isset($sent) && $sent === true) : ?>
+
+{
+  "status": "success",
+  "message": "Your data was successfully submitted"
+}
+  <?php endif; ?>
--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -2,9 +2,42 @@ import "./AltPages.css";
 import React, {useState, useEffect} from "react";
 import { Container, Button, Card, ListGroup, ListGroupItem, Row, Form, Col } from "react-bootstrap";
 import { motion, useAnimation, div, path, svg } from "framer-motion";
+// Using axios for backwards compatibility for older browsers
+// Also allows for timeout callback/property in case of connection issues
+import axios from 'axios';
 
 export default function Contact() {
   const [mounted, setMounted] = useState(false)
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [mailSent, setMailSent] = useState(false);
+  const [isError, setIsError] = useState(null);
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios({
+      method: "POST",
+      /* url: `${process.env.REACT_APP_API}`, */
+      url: "http://localhost:3002/send",
+      headers: {"content-type": application/json},
+      timeout: 5000,
+      data: {
+        name: name,
+        email: email,
+        message: message
+      }
+    })
+      .then(result => {
+        if(result.data.sent) {
+          setMailSent(result.data.sent);
+          setIsError(false);
+        }
+        else {
+          setIsError(true);
+        }
+      })
+      .catch(error => setErrror(error.message));
+  }
 
   if(!mounted){
     document.body.className="contact-body";
@@ -17,29 +50,65 @@ export default function Contact() {
       <Container className="contact-container">
           <h1>Contact Us</h1>
           <br />
-          <Form>
+          <Form onSubmit={e => { handleSubmit(e) }}>
               <Form.Row>
                   <Form.Group as={Col}>
                       <Form.Label>Your Name</Form.Label>
-                      <Form.Control as="textarea" rows={1} placeholder="Your name goes here..." name="name" required/>
+                      <Form.Control
+                        as="textarea"
+                        rows={1}
+                        placeholder="Your name goes here..."
+                        name="name"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                        required
+                      />
                   </Form.Group>
                   <Form.Group as={Col}>
                       <Form.Label>Your Email address</Form.Label>
-                      <Form.Control type="email" placeholder="name@example.com" name="email" required/>
+                      <Form.Control
+                        type="email"
+                        placeholder="name@example.com"
+                        name="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        required
+                      />
                   </Form.Group>
               </Form.Row>
+              {/* Probably don't need a subject field if we have an inquiry field
               <Form.Group>
                   <Form.Label>Subject</Form.Label>
-                  <Form.Control as="textarea" rows={1} placeholder="Subject goes here..." name="subject" required/>
-              </Form.Group>
+                  <Form.Control
+                    as="textarea"
+                    rows={1}
+                    placeholder="Subject goes here..."
+                    name="subject"
+                    value={subject}
+                    onChange={e => setSubject(e.target.value)}
+                    required
+                  />
+              </Form.Group> */}
               
               <Form.Group>
                   <Form.Label>Message content</Form.Label>
-                  <Form.Control as="textarea" placeholder="Message content goes here..." rows={3} name="message" required/>
+                  <Form.Control
+                    as="textarea"
+                    placeholder="Message content goes here..."
+                    rows={3}
+                    name="message"
+                    value={message}
+                    onChange={e => setMessage(e.target.value)}
+                    required
+                  />
               </Form.Group>
               <Button variant="outline-light" type="submit">
                   Submit
               </Button>
+              <div>
+                {mailIsSent && <div className="success">Success</div>}
+                {isError && <div className="error">Error</div>}
+              </div>
           </Form>
       </Container> 
   );
