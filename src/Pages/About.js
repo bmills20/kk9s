358d40a135168a9d82cb694023c487fdbaa2cd57
Braxton Mills <braxton25@gmail.com>
2021-08-08 20:27:00 -0500

    Mobile layout about section fixes

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -70,17 +70,18 @@ export default function About() {
             <div className="aboutContainer">
               <p className="text-left aboutPara">
                 Kinga’s K9s’ behavioral training provides you with solutions to all your dog problems, big and small.  As the founder, Kinga Niecko-Samuel has more than 14 years’ experience helping clients develop and nurture good canine behaviors.  Kinga develops puppies into well-behaved companions and solves some of the most challenging dog behaviors like separation anxiety and aggression.  Kinga is a certified trainer by the Council of Professional Dog Trainers (CPDT) and also partners with MK9S Service Dogs, an organization that trains and certifies service dogs for veterans. With Kinga’s help, you can be confident that you will be prepared for a new arrival, break any bad dog habits, and enforce and maintain good behavior in your dog.
-              </p>
-              <motion.img ref={picRef} variants={picInitial} initial={showAnimation ? "rest" : "show"} animate={ picControls } src={kingaCircle} className="circlePic" />
-                
-            </div>
-            <motion.div ref={divref} variants={certifications} initial={showAnimation ? "rest" : "show"} animate={controls} className="checkmark-container">
+                <motion.div ref={divref} variants={certifications} initial={showAnimation ? "rest" : "show"} animate={controls} className="checkmark-container">
                 <motion.p className="text-left" variants={certLines}><Checkmark />CPDT Certified</motion.p>
                 <motion.p className="text-left" variants={certLines} ><Checkmark />14+ Years of Experience</motion.p>
                 <motion.p className="text-left" variants={certLines} ><Checkmark />Specializes in behavioral in-home training</motion.p>
                 <motion.p className="text-left" variants={certLines} ><Checkmark />Partners to train service dogs for veterans</motion.p>
 
             </motion.div>
+              </p>
+              <motion.img ref={picRef} variants={picInitial} initial={showAnimation ? "rest" : "show"} animate={ picControls } src={kingaCircle} className="circlePic" />
+                
+            </div>
+            
           </Container>
       </motion.div>
     </Element>
