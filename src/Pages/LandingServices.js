d2e4b6b5cb5658fd7ee9a0bb7672500f1b65e611
Braxton Mills <braxton25@gmail.com>
2021-08-15 19:15:12 -0500

    services column cleanup

--- src/Pages/LandingServices.js
+++ src/Pages/LandingServices.js
@@ -11,6 +11,7 @@ import foundations from "../images/foundations.svg";
 import kidTraining from "../images/kidtraining.svg";
 import puppyTraining from "../images/puppytraining.svg";
 import followup from "../images/followup.svg";
+import {ReactComponent as ChevronDown} from "../images/chevron_down.svg"
 
 export default function About() {
   var controls = useAnimation();
@@ -33,24 +34,28 @@ export default function About() {
         <div className="services-container">
           <div onClick={() => setOneSelected(!oneSelected) } id="services1" className={oneSelected ? "services-column open col-sm-3" : "services-column closed col-sm-3"}>
             <img src={foundations} className="card-icon" />
-            <h3>Foundations</h3>
+            <h3>Foundations Session</h3>
             <h4>1 Session</h4>
-            <h5 className="text-muted">Works for most dogs</h5>
-            <ol>
-              <li>One to two hours</li>
-              <li>Training with the dog and owner</li>
-              <li>Ensures basic obedience of commands</li>
-              <li>Rehabilitates identified behavioral issues</li>
-              <li>Provides rehabilitation plan (homework) for the owner to achieve successful rehabilitation after the training</li>
-            </ol>
-            <Button className="btn-services">Inquire</Button>
             {oneSelected ? <p></p> : <p>Read more</p>}
+            <div className = "services-column-content">
+              <h5 className="text-muted">Works for most dogs</h5>
+              <ol>
+                <li>One to two hours</li>
+                <li>Training with the dog and owner</li>
+                <li>Ensures basic obedience of commands</li>
+                <li>Rehabilitates identified behavioral issues</li>
+                <li>Provides rehabilitation plan (homework) for the owner to achieve successful rehabilitation after the training</li>
+              </ol>
+              <Button className="btn-services">Inquire</Button>
+            </div>
+            
           </div>
           <div onClick={() => setTwoSelected(!twoSelected) } id="services2" className={twoSelected ? "services-column open col-sm-3" : "services-column closed col-sm-3"}>
             <img src={followup} className="card-icon" />
             <h3>Follow-up Session</h3>
             <h4>1 Session</h4>
-            <h5 className="text-muted">Good for a refresher or follow-up</h5>
+            <div className = "services-column-content">
+              <h5 className="text-muted">Good for a refresher or follow-up</h5>
               <ol>
                 <li>1 or more hour(s)</li>
                 <li>Assesses progress</li>
@@ -58,37 +63,44 @@ export default function About() {
                 <li>Hands-on instruction for the dog and guidance for owners </li>
               </ol>
               <Button className="btn-services">Inquire</Button>
-              {twoSelected ? <p></p> : <p>Read more</p>}
+            </div>
+            {twoSelected ? <p></p> : <p>Read more</p>}
           </div>
+        </div>
+        <div className="services-container">
           <div onClick={() => setThreeSelected(!threeSelected) } id="services3" className={threeSelected ? "services-column open col-sm-3" : "services-column closed col-sm-3"}>
             <img src={puppyTraining} className="card-icon" />
             <h3>Behavioral Training</h3>
             <h4>4 Sessions</h4>
-            <h5 className="text-muted">For puppies and dogs with difficult behavioral issues</h5>
-            <h4 style={{textAlign: "left", fontSize: "1rem", fontFamily: "firasans-lightitalic"}}>Initial Session (Up to 2 Hours):</h4>
-            <ol>
-              <li>Works with the dog and owner to ensure basic obedience of commands and rehabilitate the identified behavioral issues.</li>  
-              <li>Provides a written rehabilitation plan (homework) for the owner to achieve successful rehabilitation after the training.</li>
-            </ol>
-            <h4 style={{textAlign: "left", fontSize: "1rem", fontFamily: "firasans-lightitalic"}}>Three Follow-Up Sessions (1 hour each):</h4>
-            <ol>
-              <li>Performs three follow-on sessions to address remaining/new behavioral issues</li>
-            </ol>
-            <Button className="btn-services">Inquire</Button>
+            <div className = "services-column-content">
+              <h5 className="text-muted">For puppies and dogs with difficult behavioral issues</h5>
+              <h4 style={{textAlign: "left", fontSize: "1rem", fontFamily: "firasans-lightitalic"}}>Initial Session (Up to 2 Hours):</h4>
+              <ol>
+                <li>Works with the dog and owner to ensure basic obedience of commands and rehabilitate the identified behavioral issues.</li>  
+                <li>Provides a written rehabilitation plan (homework) for the owner to achieve successful rehabilitation after the training.</li>
+              </ol>
+              <h4 style={{textAlign: "left", fontSize: "1rem", fontFamily: "firasans-lightitalic"}}>Three Follow-Up Sessions (1 hour each):</h4>
+              <ol>
+                <li>Performs three follow-on sessions to address remaining/new behavioral issues</li>
+              </ol>
+              <Button className="btn-services">Inquire</Button>
+            </div>
             {threeSelected ? <p></p> : <p>Read more</p>}
           </div>
           <div onClick={() => setFourSelected(!fourSelected) } id="services4" className={fourSelected ? "services-column open col-sm-3" : "services-column closed col-sm-3"}>
             <img src={kidTraining} className="card-icon" />
             <h3>Kids and K9s</h3>
             <h4>4 Sessions</h4>
-            <h5 className="text-muted">Teaches Children How to Properly Handle a K9</h5>
-            <ol>
-              <li>Teaches children about dog behavior</li>
-              <li>Teaches children how to be comfortable around dogs and handle them with confidence</li>
-              <li>Your child will learn training techniques and the right time to use them to effectively handle a dog with confidence in any situation.</li> 
-              <li>Your child will have hands-on experience interacting with, handling and training a dog through four one-hour sessions.</li>
-            </ol>
-            <Button className="btn-services">Inquire</Button>
+            <div className = "services-column-content">
+              <h5 className="text-muted">Teaches Children How to Properly Handle a K9</h5>
+              <ol>
+                <li>Teaches children about dog behavior</li>
+                <li>Teaches children how to be comfortable around dogs and handle them with confidence</li>
+                <li>Your child will learn training techniques and the right time to use them to effectively handle a dog with confidence in any situation.</li> 
+                <li>Your child will have hands-on experience interacting with, handling and training a dog through four one-hour sessions.</li>
+              </ol>
+              <Button className="btn-services">Inquire</Button>
+              </div>
             {fourSelected ? <p></p> : <p>Read more</p>}
           </div>
         </div>
